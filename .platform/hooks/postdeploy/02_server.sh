7bd4435bfb69fc1b8dc076f9043b431acafca440
manishchaudhary101 <manish@codeculture.in>
2021-12-14 21:50:40 +0530

    chnages

--- .platform/hooks/postdeploy/02_server.sh
+++ .platform/hooks/postdeploy/02_server.sh
@@ -2,6 +2,10 @@
 
 chown -R ec2-user /var/app/current
 
+sudo curl -sS https://getcomposer.org/installer | sudo php
+
+sudo mv composer.phar /usr/local/bin/composer
+
 cd /var/app/current/
 
 composer require inspector-apm/inspector-laravel
