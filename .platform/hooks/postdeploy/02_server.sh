e404e181e811d33c16fd635cb89c266bdfb331c2
manishchaudhary101 <manish@codeculture.in>
2021-12-14 16:30:04 +0530

    artisan

--- .platform/hooks/postdeploy/02_server.sh
+++ .platform/hooks/postdeploy/02_server.sh
@@ -2,10 +2,6 @@
 
 chown -R ec2-user /var/app/current
 
-curl -sS https://getcomposer.org/installer | sudo php
-
-mv composer.phar /usr/local/bin/composer
-
 cd /var/app/current/
 
 composer require inspector-apm/inspector-laravel
