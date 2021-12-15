2c815c72e7a2d0942d2c42eff14533bb12d15a73
manishchaudhary101 <manish@codeculture.in>
2021-12-15 19:23:37 +0530

    okay

--- .platform/hooks/postdeploy/server.sh
+++ .platform/hooks/postdeploy/server.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-chown -R ec2-user /var/app/current
+chown -R ec2-user /var/app/staging
 
 sudo curl -sS https://getcomposer.org/installer | sudo php
 
 sudo mv composer.phar /usr/local/bin/composer
 
-cd /var/app/current/
+cd /var/app/staging/
+
+chmod 777 storage/
+
+composer update
 
 composer require inspector-apm/inspector-laravel
 
-composer update
+
 
