0dd0eb20fd7d10bf8277d6d302e4f80cf37e1e71
manishchaudhary101 <manish@codeculture.in>
2021-12-15 19:35:51 +0530

    okay

--- .platform/hooks/postdeploy/server.sh
+++ .platform/hooks/postdeploy/server.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-chown -R ec2-user /var/app/staging
+chown -R ec2-user /var/www/html
 
 sudo curl -sS https://getcomposer.org/installer | sudo php
 
 sudo mv composer.phar /usr/local/bin/composer
 
-cd /var/app/staging/
+cd /var/app/html/
 
 chmod 777 storage/
 
